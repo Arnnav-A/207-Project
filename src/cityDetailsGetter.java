abf364f972c84a9dc16ff250e8ef6a5d6c85fbcf
Arnnav Aggarwal <arnnav.aggarwal@mail.utoronto.ca>
2023-09-27 21:59:54 -0400

    Initial setup

---
+++ src/cityDetailsGetter.java
@@ -0,0 +1,9 @@
+import okhttp3.*;
+/**
+ * This (temporary) class gets the JSON details of a city using open trip map API.
+ */
+public class cityDetailsGetter {
+    public static void main(String[] args) {
+
+    }
+}
