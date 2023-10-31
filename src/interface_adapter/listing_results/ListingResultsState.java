455914d17a45175c0bb09413ae7b853290fd56d7
Nicolas Dias Martins <nicolasci2018@gmail.com>
2023-10-31 16:56:41 -0400

    implemented (not complete) the listing results view and listing results view model

--- src/interface_adapter/listing_results/ListingResultsState.java
+++ src/interface_adapter/listing_results/ListingResultsState.java
@@ -1,4 +1,19 @@
 package interface_adapter.listing_results;
 
+import entity.Listing;
+
 public class ListingResultsState {
+    private Listing listing;
+
+    public ListingResultsState(ListingResultsState copy) {listing = copy.listing;}
+
+    public ListingResultsState() {}
+
+    public Listing getListing() {
+        return listing;
+    }
+
+    public void setListing(Listing listing) {
+        this.listing = listing;
+    }
 }
--- src/interface_adapter/listing_results/ListingResultsViewModel.java
+++ src/interface_adapter/listing_results/ListingResultsViewModel.java
@@ -1,4 +1,61 @@
 package interface_adapter.listing_results;
 
+import entity.Place;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+
 public class ListingResultsViewModel {
+    public final String TITLE_LABEL = "Listing Results View.";
+    private ListingResultsState state = new ListingResultsState();
+    private String city;
+    private String filter;
+    private ArrayList<Place> points;
+
+    public ListingResultsViewModel() {
+        super();
+    }
+
+    public ListingResultsState getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getFilter() {
+        return filter;
+    }
+
+    public ArrayList<Place> getPoints() {
+        return points;
+    }
+
+    public void setState(ListingResultsState state) {
+        this.state = state;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    public void setPoints(ArrayList<Place> points) {
+        this.points = points;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
