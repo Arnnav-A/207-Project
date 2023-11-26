6eda34020d51be9ef0cf554895ed7b7f63ba46e3
Arnnav Aggarwal <arnnav.aggarwal@mail.utoronto.ca>
2023-11-26 17:18:04 -0500

    JSON implementation of file place info DAO

--- src/data_access/FilePlaceInfoDataAccessObject.java
+++ src/data_access/FilePlaceInfoDataAccessObject.java
@@ -2,10 +2,16 @@ package data_access;
 
 import entity.Place;
 import entity.PlaceFactory;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import use_case.place_info.PlaceInfoDataAccessInterface;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class FilePlaceInfoDataAccessObject implements PlaceInfoDataAccessInterface {
 
@@ -19,10 +25,37 @@ public class FilePlaceInfoDataAccessObject implements PlaceInfoDataAccessInterfa
 
     @Override
     public Place getPlaceFromName(String name) {
-        ArrayList<Double> coordinates = new ArrayList<>();
-        coordinates.add(43.6688535);
-        coordinates.add(-79.3859621);
-        return placeFactory.create("McDonald's", "675 Yonge St", coordinates,
-                "Fast Food", "Toronto");
+        Place errorPlace = placeFactory.create("Place Details Not Found", "", new ArrayList<>(), "", "");
+        try {
+            JSONObject listingJSONObject = new JSONObject(new String(Files.readAllBytes(listingFileJSON.toPath())));
+            JSONArray listingJSON;
+            try {
+                listingJSON = listingJSONObject.getJSONArray("features");
+            } catch (JSONException e) {
+                return errorPlace;
+            }
+            for (Object place: listingJSON) {
+                try {
+                    JSONObject placeProperties = ((JSONObject) place).getJSONObject("properties");
+                    String placeName = placeProperties.getString("name");
+                    if (Objects.equals(placeName, name)) {
+                        String address = placeProperties.getString("formatted");
+                        Double latitude = placeProperties.getDouble("lat");
+                        Double longitude = placeProperties.getDouble("lon");
+                        ArrayList<Double> coordinates = new ArrayList<>();
+                        coordinates.add(latitude);
+                        coordinates.add(longitude);
+                        String tags = placeProperties.getJSONArray("categories").toString();
+                        String placeCity = placeProperties.getString("city");
+                        return placeFactory.create(placeName, address, coordinates, tags, placeCity);
+                    }
+                } catch (JSONException e) {
+                    return errorPlace;
+                }
+            }
+            return errorPlace;
+        } catch (IOException e) {
+            return errorPlace;
+        }
     }
 }
