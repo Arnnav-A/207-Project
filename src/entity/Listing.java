dff021dd702a11fd2ee759adc22fc3f6122546cd
Arnnav Aggarwal <arnnav.aggarwal@mail.utoronto.ca>
2023-12-03 12:58:50 -0500

    Adding JavaDoc to Entity Interfaces

--- src/entity/Listing.java
+++ src/entity/Listing.java
@@ -2,11 +2,26 @@ package entity;
 
 import java.util.ArrayList;
 
+/**
+ * Interface for representing a search result
+ */
 public interface Listing {
 
+    /**
+     * Method to get all place entities returned
+     * @return Array list of places returned
+     */
     ArrayList<Place> getPoints();
 
+    /**
+     * Method to get the city searched
+     * @return The city used in search
+     */
     String getCity();
 
+    /**
+     * Method to get the filter searched
+     * @return The filter used in search
+     */
     String getFilter();
 }
--- src/entity/ListingFactory.java
+++ src/entity/ListingFactory.java
@@ -3,7 +3,17 @@ package entity;
 
 import java.util.ArrayList;
 
+/**
+ * Interface to create a listing entity
+ */
 public interface ListingFactory {
 
+    /**
+     * Method to create a listing entity
+     * @param points The places being stored in the listing
+     * @param city The city used in search
+     * @param filter The filter used in search
+     * @return The created listing entity
+     */
     Listing create(ArrayList<Place> points, String city, String filter);
 }
